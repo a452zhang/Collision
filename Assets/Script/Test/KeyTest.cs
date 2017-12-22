d9ced54fcf90576b6071e3df91804156d60365e1
a452zhang <34732067+a452zhang@users.noreply.github.com>
2017-12-22 15:44:40 +0800

    1

---
+++ Assets/Script/Test/KeyTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyTest : MonoBehaviour {
+    public Image image1, image2;
+    public Color color1, color2;
+	private void Start () {
+		
+	}	
+	
+	private void Update () {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            StartCoroutine(Test.FadeImage(image1,5f,color1));
+        }
+        else if (Input.GetKeyDown(KeyCode.B))
+        {
+            StartCoroutine(Test.FadeImage(image2,5f,color2));
+        }
+	}
+}
